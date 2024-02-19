c4603c1ab516dbf3edd01af4efd24de5cc4b0efe
Dydy <altis.audy@gmail.com>
2024-02-19 15:10:32 -0500

    Java Doc Pixel fini

--- src/Pixel.java
+++ src/Pixel.java
@@ -1,26 +1,36 @@
 public class Pixel {
-    private int value;
+    private int valeur;
 
     /**
      *Cette méthode est appelée lors de la création d'un nouvel object Image
-     *
-     * @param value
+     * @author Audy Altis, Arthur Andrianjafisolo, Jean-Loup Dandurand-Pominville
+     * @version 1.0
+     * @param valeur
      */
-    public Pixel(int value) {
-        this.value = value;
+
+    //Constructeur avec paramètre
+    public Pixel(int valeur) {
+        this.valeur = valeur;
+    }
+
+    //Constructeur sans paramètre
+    public Pixel(){
+        this(0);
     }
 
     /**
+     * Cette méthode retourne la valeur du pixel
      * @return value du Pixel
      */
-    public int getValue() {
-        return value;
+    public int getValeur() {
+        return valeur;
     }
 
     /**
-     * @param value
+     * Cette méthode attribut une valeur au pixel
+     * @param valeur
      */
-    public void setValue(int value) {
-        this.value = value;
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
     }
 }
---
+++ src/PixelNoirBlanc.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Audy Altis, Arthur Andrianjafisolo, Jean-Loup Dandurand-Pominville
+ * @version 1.0
+ *
+ * Cette classe est la classe fille de Pixel et elle implémente les pixels en noir et blanc
+ */
+public class PixelNoirBlanc extends Pixel {
+    public PixelNoirBlanc(int valeur) {
+        super(valeur);
+    }
+}
