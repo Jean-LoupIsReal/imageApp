1ac56520f67b8e5cdfcd5cf1aad9c4850865856f
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2024-02-20 14:03:42 -0500

    hier

--- src/Image.java
+++ src/Image.java
@@ -10,23 +10,23 @@ public class Image {
 
     private String nomFichier;
     private  String format;
+    private Pixel matricePixel[][];
     private int dimX;
     private int dimY;
-
     /**
      * Cette méthode est appelée lors de la création d'un nouvel object Image
      *
      * @param nomFichier
      * @param format
-     * @param matricePixel
      * @param dimX
      * @param dimY
      */
-    public Image(String nomFichier, String format, Pixel[][] matricePixel, int dimX, int dimY) {
+    public Image(String nomFichier, String format, Pixel matricePixel[][], int dimX, int dimY) {
         this.nomFichier = nomFichier;
         this.format = format;
         this.dimX = dimX;
         this.dimY = dimY;
+        this.matricePixel = matricePixel;
     }
 
     /**
@@ -59,6 +59,14 @@ public class Image {
     /**
      * @return la dimention en x de l'object
      */
+    public void setMatricePixel(Pixel matricePixel[][]){this.matricePixel = matricePixel;};
+    /**
+     * @return la dimention en x de l'object
+     */
+    public Pixel[][] getMatrixPixel(){return this.matricePixel;}
+    /**
+     * @return la dimention en x de l'object
+     */
     public int getDimX() {
         return dimX;
     }
@@ -87,54 +95,72 @@ public class Image {
      * @param f doit etre un fichier dans lequel l'on va chercher l'information
      * Permet de lire l'information d'un fichier
      */
-    public void lire(Image i, Files f) {}
+    public void lire(Image i, Files f) {
+
+    }
     /**
-     * @param i doit etre une image que l'on desire écrire dans un fichier
+     * @param image doit etre une image que l'on desire écrire dans un fichier
      * @param f doit etre un fichier dans lequel l'on va écrire l'information
      */
-    public void ecrire(Files f, Image i){}
+    public void ecrire(Files f, Image image){}
     /**
      * @param image1 doit etre une image que l'on desire changer
      * @param image2 doit etre une image que l'on désire dupliquer
      */
-    public void copier(Image image1, Image image2){}
+    public void copier(Image image1, Image image2){
+        image1.setDimX(image2.getDimX());
+        image1.setDimY(image2.getDimY());
+        image1.setFormat(image2.getFormat());
+        image1.setNomFichier(image2.getNomFichier());
+    }
     /**
      * @return le pixiel qui revient le plus souvent
      */
-    public Pixel couleur_predominante(Image i){ return i.getMatrixPixel()[0][0]; }
+    public Pixel couleur_predominante(Image image){ return image.getMatrixPixel()[0][0]; }
     /**
-     * @param i Contien l'image de laquel l'on désire extraire une partie
+     * @param image Contien l'image de laquel l'on désire extraire une partie
      * @param p1 x du point 1
      * @param c1 y du point 1
      * @param p2 x du point 2
      * @param c2 y du point 2
      * @return le pixiel qui revient le plus souvent
      */
-    public Image extraire(Image i, int p1, int c1, int p2, int c2){ return img;}
+    public Image extraire(Image image, int p1, int c1, int p2, int c2){ return image;}
     /**
      *
      * Cette fonction réduit la taille de l'image passé en paramettre par 2 puis l'enregistre en nouvelle image
      *
-     * @param i représente l'image d'origine
+     * @param image représente l'image d'origine
      */
-    public void reduire(Image i){}
+    public Image reduire(Image image){
+        int nouvDimX = image.getDimX()/2;
+        int nouvDimY = image.getDimY()/2;
+        Image nouvelleImg = new Image(image.getNomFichier(), image.getFormat(), image.getMatrixPixel(), nouvDimX, nouvDimY);
+        return nouvelleImg;
+    }
     /**
      *
      * Cette fonction retourne true si les deux images passé en paramettre sont identiques
      *
-     * @param i1 représente l'image 1
-     * @param i2 représente l'image 2
+     * @param image1 représente l'image 1
+     * @param image2 représente l'image 2
      */
-    public boolean sont_identiques(Image i1, Image i2){
-        return true;
+    public boolean sont_identiques(Image image1, Image image2){
+        boolean identique = true;
+        //regarde si la
+        identique = identique;
+        return identique;
     }
     /**
      *
      * Cette fonction fait une rotation de 90 degrés sur l'image reçus en paramettre puis l'enregistre en nouvelle image
      *
-     * @param i représente l'image d'origine
+     * @param image représente l'image d'origine
      */
-    public void pivoter90(Image i){
+    public void pivoter90(Image image){
+        Pixel matriceTemp[][] = new Pixel[image.getDimY()][image.getDimX()];
+        for(int  = 0; i < )
+
 
     }
 }
