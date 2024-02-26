1853e4d0cac5760d2b8e9cad2d571e3dfaa47906
Jean-LoupIsReal <131626426+Jean-LoupIsReal@users.noreply.github.com>
2024-02-26 15:55:35 -0500

    merge

--- src/ImageCouleur.java
+++ src/ImageCouleur.java
@@ -22,6 +22,41 @@ public class ImageCouleur extends Image{
     public ImageCouleur() {
         this(null,null, null,0,0);
     }
+    public ImageCouleur extraire(ImageCouleur i, int p1, int c1, int p2, int c2){
+        //Déclare les nouvelle dimmentions de l'image
+        int nouvDimX = c2-c1;
+        int nouvDimY = p2-p1;
+        //Vérifie que les chiffres entrés soient bons
+        if(nouvDimY < 0 || nouvDimX < 0)
+            //Erreur
+            System.out.print("nouvDim négatif dans extraire");
+        Pixel[][] matriceTemp= new Pixel[nouvDimY][nouvDimX];
+        //Entre les valeurs des pixels dans la matrice temporaire
+        for(int y = 0; y < nouvDimY; y++)
+        {
+            for(int x = 0; x < nouvDimX; x++)
+            {
+                matriceTemp[y][x] = i.getMatricePixel()[p1 + y][c1 + x];
+            }
+        }
+        //Entre les nouvelles informations dans un nouvel objet
+        ImageCouleur imageTemp = new ImageCouleur(i.getNomFichier(), i.getFormat(), matriceTemp, nouvDimX, nouvDimY );
+        return imageTemp;
+    }
+    public ImageCouleur reduire(Image i){
+        int nouvDimX = i.getDimX()/2;
+        int nouvDimY = i.getDimY()/2;
+        Pixel[][] matriceTemp = new Pixel[nouvDimY][nouvDimX];
+        for(int x = 0; x < nouvDimX; x++)
+        {
+            for(int y = 0; y < nouvDimY; y++)
+            {
+                matriceTemp[y][x] = i.getMatricePixel()[y][x];
+            }
+        }
+        ImageCouleur nouvelleImg = new ImageCouleur(i.getNomFichier(), i.getFormat(), matriceTemp, nouvDimX, nouvDimY);
+        return nouvelleImg;
+    }
 
     @Override
     public void ecrire(File f, Image i) throws FileNotFoundException {
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,20 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-//=================Test Jean Loup ===================
+        //=================Test Jean Loup ===================
         String fileName = "Sherbrooke_Frontenac_nuit";
         String format = "ppm";
         Image test = new Image();
         File imgFile = new File(fileName + "." + format);
         test.lire(imgFile);
         Image copieTest = new Image();
-        test.copier(copieTest, test);
+        /*test.copier(copieTest, test);
         if(test.sont_identiques(copieTest, test))
             System.out.println("sont ="); //Pour voir ce qui était affecté
         else
             System.out.println("sont !=");
-
-        System.out.println(test.couleur_predominante(test));
-
-
-//==================Test noir et blanc====================================
-
+        System.out.println(test.couleur_predominante(test));*/
+        //==================Test noir et blanc====================================
         fileName = "Sherbrooke_Frontenac_nuit";
         String formatBW = "pgm";
         test = new Image();
@@ -34,6 +30,7 @@ public class Main {
         newImgBW.ecrire(newImgFileBW, test);        //Fonctionne
 
         System.out.println("La couleur principale de l'image en noir et blanc est: " + test.couleur_predominante(test).toString());        //Fonctionne
+
         //==================Test couleur====================================
         String formatC = "ppm";
         Image test2 = new Image();
@@ -47,6 +44,17 @@ public class Main {
 
         newImgC.ecrire(newImgFileC, test2);        //Fonctionne
 
-        System.out.println("La couleur principale de l'image couleur  est: " + test2.couleur_predominante(test2).toString());        //Fonctionne
+        System.out.println("La couleur principale de l'image couleur  est: " + test2.couleur_predominante(test2).toString());
+
+        //==================================== Test extraire/reduire ===================================
+
+        newImgBW.lire(newImgFileBW);
+        ImageNoirEtBlanc test3 = newImgBW.extraire(newImgBW,0 , 119, 192, 255);        //Fonctionne
+        File fileTest3 = new File("testExtraire.pgm");
+        test3.ecrire(fileTest3, test3);//Fonctionne
+
+        System.out.println("La couleur principale de l'image en noir et blanc est: " + test.couleur_predominante(test).toString());
+        // Fonctionne
+        ImageNoirEtBlanc testReduire = newImgBW.reduire(newImgC);
     }
 }
